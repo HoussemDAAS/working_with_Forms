e74b5d0d12c71a9b750089ccaafa98f359b56d16
HoussemDAAS <houssemdaas2@gmail.com>
2024-03-11 14:58:28 +0100

    validating input on lost focus with onBlur and useState

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,28 +4,46 @@ export default function Login() {
     email: "",
     password: "",
   });
-// const email= useRef();
-// const password= useRef();
+  const [isBlur, setIsBlur] = useState({
+    email: false,
+    password: false,
+  });
+  // const email= useRef();
+  // const password= useRef();
 
-const handleInputChange = (identifier,value) => {
-  setEntredValue((prevEntredValue) => {
-    return {
-      ...prevEntredValue,
-      [identifier]: value,
-    };
-  })
-}
-const EmailIsInvalid=EntredValue.email!=='' && !EntredValue.email.includes('@');
-const PasswordIsInvalid=EntredValue.password!=='' && EntredValue.password.trim().length<5;
+  const EmailIsInvalid = isBlur.email && !EntredValue.email.includes("@");
+  const PasswordIsInvalid =
+    isBlur.password && EntredValue.password.trim().length < 5;
   const handdleSubmit = (event) => {
     event.preventDefault();
-// const entredEmail= email.current.value;
-// const entredPassword= password.current.value;
-// console.log(entredEmail,entredPassword)
-// email.current.value='';
-// password.current.value='';
+    // const entredEmail= email.current.value;
+    // const entredPassword= password.current.value;
+    // console.log(entredEmail,entredPassword)
+    // email.current.value='';
+    // password.current.value='';
   };
-
+  const handleInputChange = (identifier, value) => {
+    setEntredValue((prevEntredValue) => {
+      return {
+        ...prevEntredValue,
+        [identifier]: value,
+      };
+    });
+    setIsBlur((prevIsBlur) => {
+      return {
+        ...prevIsBlur,
+        [identifier]: false,
+      };
+    })
+  };
+  const handleInputBlur = (identifier) => {
+    setIsBlur((prevIsBlur) => {
+      return {
+        ...prevIsBlur,
+        [identifier]: true,
+      };
+    });
+  };
   return (
     <form onSubmit={handdleSubmit}>
       <h2>Login</h2>
@@ -33,16 +51,40 @@ const PasswordIsInvalid=EntredValue.password!=='' && EntredValue.password.trim()
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" /*ref={email}*/  onChange={(event)=>handleInputChange('email',event.target.value)} value={EntredValue.email}  />
-       <div className="control-error">
-       {EmailIsInvalid && <p className="error-text">Please enter a valid email</p>}
-       </div>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            /*ref={email}*/ onChange={(event) =>
+              handleInputChange("email", event.target.value)
+            }
+            value={EntredValue.email}
+            onBlur={() => handleInputBlur("email")}
+          />
+          <div className="control-error">
+            {EmailIsInvalid && (
+              <p className="error-text">Please enter a valid email</p>
+            )}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" /*ref={password}*/  onChange={(event)=>handleInputChange('password',event.target.value)} value={EntredValue.password} />
-       <div className="control-error">{PasswordIsInvalid && <p className="error-text">Please enter a valid password</p>}</div>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            /*ref={password}*/ onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+            value={EntredValue.password}
+            onBlur={() => handleInputBlur("password")}
+          />
+          <div className="control-error">
+            {PasswordIsInvalid && (
+              <p className="error-text">Please enter a valid password</p>
+            )}
+          </div>
         </div>
       </div>
 
