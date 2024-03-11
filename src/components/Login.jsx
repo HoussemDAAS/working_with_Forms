1a7b8c90db50318a7635bb5a480b5c98d1f9f1af
HoussemDAAS <houssemdaas2@gmail.com>
2024-03-11 13:21:12 +0100

    managing and getting user input via use State

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,43 @@
+import React, { useState } from "react";
 export default function Login() {
+  const [EntredValue, setEntredValue] = useState({
+    email: "",
+    password: "",
+  });
+
+
+const handleInputChange = (identifier,value) => {
+  setEntredValue((prevEntredValue) => {
+    return {
+      ...prevEntredValue,
+      [identifier]: value,
+    };
+  })
+}
+  const handdleSubmit = (event) => {
+    event.preventDefault();
+    console.log("email"+EntredValue.email+"password"+EntredValue.password);
+  };
   return (
-    <form>
+    <form onSubmit={handdleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={(event)=>handleInputChange('email',event.target.value)} value={EntredValue.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={(event)=>handleInputChange('password',event.target.value)} value={EntredValue.password} />
         </div>
-
-        
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
+        <button className="button button-flat" type="reset">
+          Reset
+        </button>
         <button className="button">Login</button>
       </p>
     </form>
